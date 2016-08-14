9026ca776c9994c28e5439793c943bc4f1606111
komali2 <rogersjcaleb@gmail.com>
2016-08-14 13:42:36 -0700

    added another console example

--- node/console.js
+++ node/console.js
@@ -4,9 +4,25 @@ const Console = console.Console;
 //turns out console can be more than just a way to put text in the terminal
 //you can make a custom console that for example logs out to a file
 
-const output = fs.createWriteStream('./testlog.txt');
+// const output = fs.createWriteStream('./testlog.txt');
 
-const logger = new Console(output);
+// const logger = new Console(output);
 
-var count = 5;
-logger.log('count: %d', count);
+// var count = 5;
+// logger.log('count: %d', count);
+
+
+//////// 
+//did you know about console timers? i didn't
+
+console.time('test');
+
+for(var i = 0; i < 1000; i++){
+    ;
+} 
+
+console.timeEnd('test');
+//test: 1.246ms
+
+//you can also stacktrace, this is turning into a full blown debug
+// console.trace('to right here');
