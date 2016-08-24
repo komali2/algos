66bf53721506164ee0d0dc4dc4996f83f9531824
komali2 <rogersjcaleb@gmail.com>
2016-08-24 08:45:00 -0700

    wip: linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -1,6 +1,32 @@
-class LinkedList{
-    constructor(val){
-        this.head = null;
-        this.tail = null;
+class LinkedList {
+  constructor(node) {
+    this.head = node;
+    
+  }
+
+  addFirst(val) {
+    let temp = this.head;
+    this.head = new Node(val);
+    this.head.next = temp;
+  }
+
+  
+
+  removeFromTail(){
+    let current = this.head;
+    let prev;
+    while(current.next){
+      prev = current;
+      current = current.next;
     }
+    prev.next = null;
+    return current.val;
+  }
+}
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
 }
