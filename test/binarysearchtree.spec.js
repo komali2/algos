5660bf102e30b46b87cbb4246c9a6e4bad699886
komali2 <rogersjcaleb@gmail.com>
2016-08-24 09:16:20 -0700

    test: finished out initial tests for linkedList

--- test/binarysearchtree.spec.js
+++ test/binarysearchtree.spec.js
@@ -1,81 +1,81 @@
-describe('Binary search tree', ()=>{
-    it('should exist', ()=>{
-        expect(binarySearchTree).toBeDefined();
-    });
-    var tree = new binarySearchTree(3);
-    it('should be able to create new versions of itself', ()=>{
-        expect(tree instanceof binarySearchTree).toBe(true);
-    })
+describe('Binary search tree', () => {
+  it('should exist', () => {
+    expect(binarySearchTree).toBeDefined();
+  });
+  var tree = new binarySearchTree(3);
+  it('should be able to create new versions of itself', () => {
+    expect(tree instanceof binarySearchTree).toBe(true);
+  })
 
 
-    it('should be able to add new nodes', ()=>{
-        expect(tree.addNode).toBeDefined();
-        for(var i = 0; i < 5; i++){
-            if(i !== 3){
-                tree.addNode(i);
-            }
-        }
-        expect(tree.right.val).toBe(4);
-        expect(tree.left.val).toBe(0);
-    });
+  it('should be able to add new nodes', () => {
+    expect(tree.addNode).toBeDefined();
+    for (var i = 0; i < 5; i++) {
+      if (i !== 3) {
+        tree.addNode(i);
+      }
+    }
+    expect(tree.right.val).toBe(4);
+    expect(tree.left.val).toBe(0);
+  });
 
-    it('should add nodes in the right order', ()=>{
-        var tree2 = new binarySearchTree(5);
-        tree2.addNode(2);
-        expect(tree2.left.val).toBe(2);
-        tree2.left.addNode(3);
-        expect(tree2.left.right.val).toBe(3);
-        tree2.left.right.addNode(4);
-        expect(tree2.left.right.right.val).toBe(4);
-    });
+  it('should add nodes in the right order', () => {
+    var tree2 = new binarySearchTree(5);
+    tree2.addNode(2);
+    expect(tree2.left.val).toBe(2);
+    tree2.left.addNode(3);
+    expect(tree2.left.right.val).toBe(3);
+    tree2.left.right.addNode(4);
+    expect(tree2.left.right.right.val).toBe(4);
+  });
 
-    var tree3 = new binarySearchTree(50);
-    it('should be able to search for values', ()=>{
-        tree3.addNode(35);
-        tree3.left.addNode(45);
-        tree3.left.right.addNode(40);
-        tree3.left.right.addNode(46);
-        tree3.left.addNode(20);
-        tree3.left.left.addNode(25);
-        tree3.left.left.addNode(19);
-        tree3.addNode(75);
-        tree3.right.addNode(60);
-        tree3.right.addNode(80);
-        expect(tree3.contains(35)).toBe(true);
-        expect(tree3.contains(45)).toBe(true);
-        expect(tree3.contains(40)).toBe(true);
-        expect(tree3.contains(46)).toBe(true);
-        expect(tree3.contains(20)).toBe(true);
-        expect(tree3.contains(25)).toBe(true);
-        expect(tree3.contains(19)).toBe(true);
-        expect(tree3.contains(1)).toBe(false);
-        expect(tree3.contains(100)).toBe(false);
-        expect(tree3.contains(51)).toBe(false);
-        expect(tree3.contains(41)).toBe(false);
-    });
+  var tree3 = new binarySearchTree(50);
+  it('should be able to search for values', () => {
+    tree3.addNode(35);
+    tree3.left.addNode(45);
+    tree3.left.right.addNode(40);
+    tree3.left.right.addNode(46);
+    tree3.left.addNode(20);
+    tree3.left.left.addNode(25);
+    tree3.left.left.addNode(19);
+    tree3.addNode(75);
+    tree3.right.addNode(60);
+    tree3.right.addNode(80);
+    expect(tree3.contains(35)).toBe(true);
+    expect(tree3.contains(45)).toBe(true);
+    expect(tree3.contains(40)).toBe(true);
+    expect(tree3.contains(46)).toBe(true);
+    expect(tree3.contains(20)).toBe(true);
+    expect(tree3.contains(25)).toBe(true);
+    expect(tree3.contains(19)).toBe(true);
+    expect(tree3.contains(1)).toBe(false);
+    expect(tree3.contains(100)).toBe(false);
+    expect(tree3.contains(51)).toBe(false);
+    expect(tree3.contains(41)).toBe(false);
+  });
 
-    it('should place nodes properly', ()=>{
-        var tree4= new binarySearchTree(50);
-        tree4.addNode(60);
-        tree4.addNode(55);
-        tree4.addNode(57);
-        tree4.addNode(56);
-        tree4.addNode(54);
-        expect(tree4.contains(60)).toBe(true);
-        expect(tree4.contains(55)).toBe(true);
-        expect(tree4.contains(57)).toBe(true);
-        expect(tree4.contains(56)).toBe(true);
-        expect(tree4.contains(54)).toBe(true);
-        expect(tree4.contains(1)).toBe(false);
-    })
+  it('should place nodes properly', () => {
+    var tree4 = new binarySearchTree(50);
+    tree4.addNode(60);
+    tree4.addNode(55);
+    tree4.addNode(57);
+    tree4.addNode(56);
+    tree4.addNode(54);
+    expect(tree4.contains(60)).toBe(true);
+    expect(tree4.contains(55)).toBe(true);
+    expect(tree4.contains(57)).toBe(true);
+    expect(tree4.contains(56)).toBe(true);
+    expect(tree4.contains(54)).toBe(true);
+    expect(tree4.contains(1)).toBe(false);
+  })
 
-    xit('should be able to remove nodes', ()=>{
-        tree3.removeNode(40);
-        tree3.removeNode(80);
-        tree3.removeNode(20);
-        expect(tree3.contains(40)).toBe(false);
-        expect(tree3.contains(80)).toBe(false);
-        expect(tree3.contains(20)).toBe(false);
-        
-    });
+  xit('should be able to remove nodes', () => {
+    tree3.removeNode(40);
+    tree3.removeNode(80);
+    tree3.removeNode(20);
+    expect(tree3.contains(40)).toBe(false);
+    expect(tree3.contains(80)).toBe(false);
+    expect(tree3.contains(20)).toBe(false);
+
+  });
 });
--- test/linkedList.spec.js
+++ test/linkedList.spec.js
@@ -1,5 +1,62 @@
-describe('Linked List', ()=>{
-    it('should exist', ()=>{
-        expect(LinkedList).toBeDefined();
-    });
+describe('Linked List', () => {
+  it('should exist', () => {
+    expect(LinkedList).toBeDefined();
+  });
+
+  var list = new LinkedList(3);
+  it('should be able to instatiate linkedlists', () => {
+    expect(list instanceof LinkedList).toBe(true);
+  });
+  //3
+  it('should have a functioning addFirst method', () => {
+    expect(list.addFirst).toBeDefined();
+    list.addFirst(2);
+    expect(list.getFirst()).toBe(2);
+  });
+  //2, 3
+  it('should have a functioning addLast method', () => {
+    expect(list.addLast).toBeDefined();
+    list.addLast(5);
+    expect(list.addLast()).toBe(5);
+  });
+  //2, 3, 5
+  it('should have a functioning getFirst method', () => {
+    expect(list.getFirst).toBeDefined();
+    expect(list.getFirst()).toBe(2);
+    list.addFirst(1);
+    expect(list.getFirst()).toBe(1);
+  });
+  //1, 2, 3, 5
+  it('should have a functioning getLast method', () => {
+    expect(list.getLast).toBeDefined();
+    expect(list.getLast()).toBe(5);
+    list.addLast(9);
+    expect(list.getLast()).toBe(9);
+  });
+  //1, 2, 3, 5, 9
+  it('should have a functioning removeFirst method', () => {
+    expect(list.removeFirst).toBeDefined();
+    expect(list.removeFirst()).toBe(1);
+    expect(list.getFirst()).toBe(2);
+  });
+  //2, 3, 5, 9
+  it('should have a functioning removeLast method', () => {
+    expect(list.removeLast).toBeDefined();
+    expect(list.removeLast()).toBe(9);
+    list.addLast(20);
+    expect(list.getLast()).toBe(20);
+  });
+  //2, 3, 5, 20
+  it('should have a functioning size method', () => {
+    expect(list.size).toBeDefined();
+    expect(list.size()).toBe(4);
+    list.addLast(50);
+    expect(list.size()).toBe(5);
+    list.addFirst(25);
+    expect(list.size()).toBe(6);
+    list.removeFirst();
+    expect(list.size()).toBe(5);
+    list.removeLast();
+    expect(list.size()).toBe(4);
+  });
 });
