a50262ccbd2333ca0c3c8a97807e4d61b616628e
komali2 <rogersjcaleb@gmail.com>
2016-09-06 11:24:53 -0700

    dev: psuedocode

---
+++ stringMath.js
@@ -0,0 +1,10 @@
+var stringMath = function(str){
+    //we want to break down problem into 'chunks' of positive and negative
+    //numbers that can be added to a total
+
+    function recurse(input){
+        //the base case would be a negative or positive number
+
+        //otherwise, we need to extract a negative or positive number
+    }
+}
