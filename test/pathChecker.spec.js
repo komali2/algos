cf02fed41246e575d5045a420b1a1c94d4dbc85b
komali2 <rogersjcaleb@gmail.com>
2016-09-06 09:54:57 -0700

    dev: set up tests

--- test/pathChecker.spec.js
+++ test/pathChecker.spec.js
@@ -1,6 +1,42 @@
 describe('path checker', () => {
   it('should be defined', () => {
     expect(pathChecker).toBeDefined();
+  });
+  it('should return a bool', ()=>{
+    expect(pathChecker([[1, 2],[2, 1]], [3, 6, 5])).toEqual(false);
+  })
+  it('should return true if a path exists', ()=>{
+    var board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+    var pathGood = [1, 4, 5];
+    expect(pathChecker(board, pathGood)).toEqual(true);
+    pathGood = [3, 6, 9];
+    expect(pathChecker(board, pathGood)).toEqual(true);
+    pathGood = [9, 8, 5];
+    expect(pathChecker(board, pathGood)).toEqual(true);
+    pathGood = [6, 5, 4];
+    expect(pathChecker(board, pathGood)).toEqual(true);
+    pathGood = [1, 2, 3];
+    expect(pathChecker(board, pathGood)).toEqual(true);
+  });
+  it('should return false if path doesnt exist', ()=>{
+    var board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+    var pathBad = [3, 6, 8];
+    expect(pathChecker(board, pathBad)).toEqual(true);
+    pathBad = [2, 1, 3];
+    expect(pathChecker(board, pathBad)).toEqual(true);
+    pathBad = [6, 9, 1];
+    expect(pathChecker(board, pathBad)).toEqual(true);
+    pathBad = [1, 9, 7];
+    expect(pathChecker(board, pathBad)).toEqual(true);
+    pathBad = [1, 2, 6];
   })
 
 })
