0eec8994c9b7959404ff3c09886f23d09d23db9c
komali2 <rogersjcaleb@gmail.com>
2016-09-06 12:14:58 -0700

    dev: cleanup

--- rotateArray.js
+++ rotateArray.js
@@ -12,20 +12,3 @@ var rotateArray = function(matrix){
     }
     return out;
 }
-
-    var matrix = 
-    [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [9, "A", "B", "C"],
-      ["D", "E", "F", "G"]
-    ];
-
-    var rotatedMatrix = 
-      [ 
-        [ "D", 9, 5, 1 ], 
-        [ "E", "A", 6, 2 ], 
-        [ "F", "B", 7, 3 ], 
-        [ "G", "C", 8, 4 ] 
-      ]
-rotateArray(matrix);
