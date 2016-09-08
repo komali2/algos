6a152429d2b70f8047b04a2172658229bc26d7b5
komali2 <rogersjcaleb@gmail.com>
2016-09-08 15:21:54 -0700

    core: solved

--- shuffle.js
+++ shuffle.js
@@ -1,3 +1,16 @@
-var shuffle = function(){
-  
+var shuffle = function(arr){
+  var out = [];
+  arr = Array.prototype.slice.call(arr);
+  while(arr.length){
+    var rand = random(0, arr.length - 1 );
+    out.push(arr[rand]);
+    arr.splice(rand, 1);
+  }
+  return out;
+}
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
