a9f1431223c0552b760a243a40539a797dde7988
komali2 <rogersjcaleb@gmail.com>
2016-09-08 14:48:25 -0700

    core: added other methods

--- flattenArray.js
+++ flattenArray.js
@@ -13,3 +13,22 @@ var flattenArray = function(arr){
   return out;
 }
 
+//only works one level deep
+// var flattenArray = function(arr){
+//   return [].concat.apply([], arr);
+// }
+
+//only works one level deep
+// var flattenArray = function(arr){
+//   var out = [];
+//   for(var i = 0; i < arr.length; i++){
+//     if(Array.isArray(arr[i])){
+//       for(var j = 0; j < arr[i].length; j++){
+//         out.push(arr[i][j]);
+//       }
+//     } else{
+//       out.push(arr[i]);
+//     }
+//   }
+//   return out;
+// }
